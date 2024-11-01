4898d6228a1f2586c6165505479cc9c7ea5ace5c
Ugolovka <Medusa_Gangrena@tutamail.com>
2024-11-01 16:04:17 +0300

    full

--- plot.py
+++ plot.py
@@ -1,8 +1,10 @@
+# Импортируем библиотеки
 import matplotlib.pyplot as plt
 import math
 import numpy as np
 t = np.arange(0,2.5,0.1)
+# Уравнение функции
 y1 = np.sin(math.pi*t)
 plt.plot(t,y1,'b-',)
-plt.plot(t,y1,'b*')
+plt.plot(t,y1,'b*') 
 plt.show()
